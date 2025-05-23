5ba6f52ae1a8b2c65288d704ff742cbd93733df4
Richard Benny <richardvictorbenny@gmail.com>
2025-05-23 23:00:33 +0100

    pipeline test 2

--- API/EventManagement/EventManagement.Web/Controllers/WeatherForecastController.cs
+++ API/EventManagement/EventManagement.Web/Controllers/WeatherForecastController.cs
@@ -19,7 +19,7 @@ namespace EventManagement.Web.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> GetWeatherData()
+        public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
